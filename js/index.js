363f714a4ca20f210b511785f041ae4ec9e4b787
cjones1047 <cjmj1047@gmail.com>
2022-06-17 11:10:00 -0500

    Started working or answer word list

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// alright lets just create our own wordlist lol:
+// 1. mountain
+// 2. fish
+// 3. bench
+// 4. sega
+// 5. mario
+// 6. ragnarok
+// 7. tree
+// 8. recorder
+// 9. treadmill
+// 10. 
